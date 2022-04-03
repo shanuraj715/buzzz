d539157937d6f850380c8b72f821e09847b47624
Shanu Raj <shanuraj715@gmail.com>
2022-04-04 01:04:13 +0530

    Replaced css files to scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { createRoot } from 'react-dom/client';
 import './index.css';
-import './assets/css/layout.css'
+import './assets/css/layout.scss'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
