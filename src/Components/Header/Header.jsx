47eff509e7686a265ca5c92fde1f26c939e24bd7
shobitkhatri05 <shobitkhatri62@gmail.com>
2022-04-13 00:47:28 +0530

    Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import "./Header.css";
 import Icon from "../../Components/FontAwesome/FontAwesome";
 import Image from '../../assets/images/50x50.png'
-import Img from '../../assets/images/logo-text.png'
+// import Img from '../../assets/images/logo-text.png'
 
 function Header() {
   return (
     <>
       <header className="container-fluid customclass">
-        <div>
-          <img  classname ="logo-img" src ={Img} alt=""/>
+        <div className="logo-img">
+         <h1>jdjsdjsd</h1>
         </div>
         <div className="content">
           <ul>
@@ -17,7 +17,7 @@ function Header() {
               <div className="header-user-img">
                 <img src={Image} alt="" />
               </div>
-              <span>Shobit Khannatri</span>
+              <span className="username">Shobit Khannatri</span>
             </li>
             <li>
               <button className="circular-icon-btn header-btn">
